8ea7f489e3e26d88d72c6594bc49625634b10b0f
jluisdeveloper <jluis.developer@gmail.com>
2021-08-05 01:31:02 -0500

    add grid css in text description from portfolio

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import './styles/portfolio.scss'
+
 const PortfolioPage = () => {
   return (
-    <div style={{ height: '100vh' }}>
-       
+    <div
+      id="portfolio"
+      className="portfolio_container"
+    >
+      <div className="portfolio_intro_container" >
+        <div className="portfolio_wrapper_text">
+          <div className="portfolio_intro_title_wrapper" >
+            <h3 className="portfolio_intro_title">
+              Sabemos lo que necesitas <br />
+              para <strong className="strong_std">CRECER </strong>
+            </h3>
+          </div>
+          <div className="portfolio_intro_text_wrapper">
+            <p className="portfolio_intro_text">
+              Combinamos conocimiento, experiencia en el <strong>Diseño Web</strong> para impulsar las ventas de tu empresa, o negocio; operamos en todo el <strong>Perú</strong> con nuestro lema <strong> CREZCAMOS JUNTOS</strong> <br />
+
+              Cuando su herramienta favorita es un martillo, cada problema parece un clavo. EL enfoque independiente, transparente y honesto de <strong>STUDIOAQP</strong> garantiza que siempre usemos las 'herramientas' adecuadas para resolver su problema comercial.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
